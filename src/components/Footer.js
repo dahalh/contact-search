487fa5440d04b62ebeaa6e47c89314ea6f6f1351
dahalh <dahalh@outlook.com>
2022-09-03 20:03:05 +1000

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="bg-light text-danger py-4 text-center">Footer</div>;
+  return (
+    <div className="bg-light text-danger py-4 text-center">
+      &copy; Resonate Solutions
+    </div>
+  );
 };
 
 export default Footer;
